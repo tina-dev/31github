2f2d891fc62809957a4d77f0302d0197699ef43c
tinatemishova <tina.temishova@gmail.com>
2023-11-19 13:59:31 -0700

    method class added.

---
+++ src/MethodsEncapsulation.java
@@ -0,0 +1,13 @@
+public class MethodsEncapsulation {
+
+    public static void main(String [] args) {
+        System.out.println(calculateAnswer(5,4));
+
+
+
+    }
+
+    static double calculateAnswer(double length, double width) {
+        return length + width;
+    }
+}

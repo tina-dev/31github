cffa42afcb3ab5a416562347c0eb9e467ae724ac
tinatemishova <tina.temishova@gmail.com>
2023-11-22 13:13:05 -0700

    Changes in StepDefinitions class

---
+++ src/com/java/testing/Calculator.java
@@ -0,0 +1,7 @@
+package com.java.testing;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
--- src/com/java/testing/StepDefinitions.java
+++ src/com/java/testing/StepDefinitions.java
@@ -1,4 +1,13 @@
+import com.java.testing.Calculator;
+
 package com.java.testing;
 
 public class StepDefinitions {
+
+    private Calculator calculator;
+    private int result;
+
+    public void i_have_a_calculator () {
+
+    }
 }

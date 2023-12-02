3f56bd150ec2cd871ec0b635dc460f6bcae9a999
tinatemishova <tina.temishova@gmail.com>
2023-12-02 14:34:33 -0700

    Hackerrank challenge solved.

---
+++ src/com/java/hackerrank/JavaLoops.java
@@ -0,0 +1,19 @@
+package com.java.hackerrank;
+
+import java.util.Scanner;
+
+public class JavaLoops {
+    public static void main(String [] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int i = 0; i<t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+        }
+        System.out.println(t);
+
+
+    }
+}

0c418b86a10466c05ddd6c54473b86ce22ef81cf
tinatemishova <tina.temishova@gmail.com>
2023-12-06 11:38:54 -0700

    Arrays Basic

---
+++ src/com/java/ArraysBasic.java
@@ -0,0 +1,11 @@
+package com.java;
+
+public class ArraysBasic {
+    public static void main(String [] args) {
+        int [] nums = new int[5];
+        nums[0] = 45;
+        nums[1] = 40;
+
+        System.out.println(nums[0]);
+    }
+}

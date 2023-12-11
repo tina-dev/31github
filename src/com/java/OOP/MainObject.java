cc7a856547d54eec8315c977c4b2fcd047d7f4b1
tinatemishova <tina.temishova@gmail.com>
2023-12-11 10:55:46 -0700

    Some modifications

--- src/com/java/OOP/MainObject.java
+++ src/com/java/OOP/MainObject.java
@@ -3,12 +3,15 @@ package com.java.OOP;
 public class MainObject {
     public static void main(String [] args) {
 
-        VehicleMain tesla = new VehicleMain();
-        tesla.speed = 250.5f;
-        tesla.weight = 2500;
-        tesla.color = "White";
-        tesla.coordinate = new int[] {40, 0, 0};
+        VehicleMain tesla = new VehicleMain(250.5f, 2500, "White", new int[] {40, 0, 0});
+//        tesla.speed = 250.5f;
+//        tesla.weight = 2500;
+//        tesla.color = "White";
+//        tesla.coordinate = new int[] {40, 0, 0};
 
-        System.out.println("Tesla speed: " + tesla.speed);
+        System.out.println("Tesla speed: " + tesla.color);
+
+        VehicleMain bmw = new VehicleMain(200, "White");
+        System.out.println(bmw.weight);
     }
 }
--- src/com/java/OOP/VehicleMain.java
+++ src/com/java/OOP/VehicleMain.java
@@ -7,4 +7,16 @@ public class VehicleMain {
     public String color;
     public int[] coordinate;
 
+    public VehicleMain(float speed, int weight, String color, int [] coordinate) {
+        this.speed = speed;
+        this.weight = weight;
+        this.color = color;
+        this.coordinate = coordinate;
+    }
+
+    public VehicleMain(int weight, String color) {
+        this.weight = weight;
+        this.color = color;
+    }
+
 }

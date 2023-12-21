3090513463022f8e37520054bf5c289d5d288bbe
tinatemishova <tina.temishova@gmail.com>
2023-12-20 23:05:43 -0700

    Intro

--- src/com/java/algorithms/IntroClass.java
+++ src/com/java/algorithms/IntroClass.java
@@ -8,6 +8,7 @@ public class IntroClass {
 
 
         public static void findNemo (String[] array) {
+        //long start = System.currentTimeMillis();
             for (int i = 0; i < array.length; i++) {
                 if (array[i].equals("nemo")) {
                     System.out.println("Found Nemo");
@@ -15,6 +16,8 @@ public class IntroClass {
                     System.out.println("not matching");
                 }
             }
+            //long finish = System.currentTimeMillis();
+            //System.out.println(finish - start);
 
         }
 

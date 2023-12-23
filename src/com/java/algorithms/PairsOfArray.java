7a461e698f590cf908c0668aa022c1ca19710bdc
tinatemishova <tina.temishova@gmail.com>
2023-12-22 23:25:52 -0700

    Intro

--- src/com/java/algorithms/PairsOfArray.java
+++ src/com/java/algorithms/PairsOfArray.java
@@ -2,11 +2,11 @@ package com.java.algorithms;
 
 public class PairsOfArray {
     public static void main(String [] args) {
-        int [] numbers = new int[] {1,2,3,4,5};
+        String [] numbers = new String[] {"a", "b", "c"};
         logAllPairs(numbers);
     }
 
-    public static void logAllPairs(int [] num) {
+    public static void logAllPairs(String [] num) {
         for(int i = 0; i <num.length; i++) {
             for(int j = 0; j < num.length; j++) {
                 System.out.println(num[i] + " " + num[j]);

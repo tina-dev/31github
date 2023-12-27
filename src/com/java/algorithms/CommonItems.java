072091dd8e355ce6bef34ae26ccc77c53f35df6a
tinatemishova <tina.temishova@gmail.com>
2023-12-27 13:33:07 -0700

    Has Pairs with sum challenge

--- src/com/java/algorithms/CommonItems.java
+++ src/com/java/algorithms/CommonItems.java
@@ -18,4 +18,6 @@ public class CommonItems {
         }
         return false;
     }
+
+
 }
---
+++ src/com/java/algorithms/PairWithSum.java
@@ -0,0 +1,22 @@
+package com.java.algorithms;
+
+public class PairWithSum {
+    public static void main(String [] args) {
+        int [] nums = {2,3,4,4};
+        int summ = 8;
+        System.out.println(hasPairWithSum(nums, summ));
+
+    }
+
+    public static boolean hasPairWithSum(int [] arr, int sum) {
+        int len = arr.length;
+        for(int i = 0; i < len-1; i++) {
+            for(int j = i+1; j < len; j++) {
+                if(arr[j] + arr[j] == sum) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}

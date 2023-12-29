f1c6e476028c0c32aa1dce3419551d6b001ef723
tinatemishova <tina.temishova@gmail.com>
2023-12-29 11:44:07 -0700

    Arrays Intro

---
+++ src/com/java/algorithms/Arrays/Intro.java
@@ -0,0 +1,22 @@
+package com.java.algorithms.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Intro {
+    public static void main(String [] args) {
+        List<String> list = new ArrayList<String>();
+
+        list.add("a");
+        list.add("b"); //O(1)
+        list.add("c");
+
+        list.remove(2); //O(1)
+
+        list.add(0, "d"); //O(1)
+
+
+
+        System.out.println(list);
+    }
+}

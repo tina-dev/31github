2dc32f3f199f7fb2ff1593a52b88fb7252380a7f
tinatemishova <tina.temishova@gmail.com>
2024-01-20 22:24:57 -0700

    Containst duplicate solution

--- src/com/java/algorithms/Arrays/ContainsDuplicate.java
+++ src/com/java/algorithms/Arrays/ContainsDuplicate.java
@@ -8,7 +8,6 @@ import java.util.Set;
 public class ContainsDuplicate {
     public static void main(String [] args) {
         System.out.println(containsDuplicate(new int [] {1, 2, 4, 3}));
-
     }
 
     public static boolean containsDuplicate(int [] nums) {

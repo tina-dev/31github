dc038c570b647fee20062407823c8689839f2688
tinatemishova <tina.temishova@gmail.com>
2024-02-03 15:35:42 -0700

    Playing with Linked List

--- src/com/java/algorithms/HashMap/FirstRecurring.java
+++ src/com/java/algorithms/HashMap/FirstRecurring.java
@@ -7,6 +7,7 @@ public class FirstRecurring {
     public static void main(String [] args) {
         int [] num = {1, 2, 3, 4, 5, 2};
         recurring(num);
+        System.out.println(recurring2(num));
 
     }
 
@@ -22,5 +23,17 @@ public class FirstRecurring {
                 set.add(nums[i]);
             }
         }
+    }
+
+
+    public static int recurring2(int [] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] == nums[j]) {
+                    return nums[i];
+                }
+            }
+        }
+        return 0;
     }
 }
---
+++ src/com/java/algorithms/LinkedList/LinkedListIntro.java
@@ -0,0 +1,16 @@
+package com.java.algorithms.LinkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListIntro {
+    public static void main(String [] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        for( int lists: list) {
+            System.out.println(lists);
+
+        }
+    }
+}

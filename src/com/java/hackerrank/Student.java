5e71b645b7d10d49a8af7153d55320724d5b47b5
tinatemishova <tina.temishova@gmail.com>
2024-03-10 14:10:08 -0600

    Min common value solution

--- src/com/java/hackerrank/Student.java
+++
@@ -1,2 +0,0 @@
-package com.java.hackerrank;public class Student {
-}
---
+++ src/com/java/hackerrank/Students.java
@@ -0,0 +1,7 @@
+package com.java.hackerrank;
+
+public class Student {
+    public static void main(String [] args) {
+
+    }
+}

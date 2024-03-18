85c98e0480b63560d6135bfc8d76ed213c694f54
tinatemishova <tina.temishova@gmail.com>
2024-03-18 10:58:52 -0600

    Reverse Char

--- src/com/java/leetcode/ReverseChar.java
+++ src/com/java/leetcode/ReverseChar.java
@@ -1,11 +1,27 @@
 package com.java.leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ReverseChar {
     public static void main(String[] args) {
+        char [] ch = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(ch);
 
     }
 
-    public void reverseString(char [] s) {
+    public static void reverseString(char [] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
 
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+        System.out.println(Arrays.toString(s));
     }
 }
